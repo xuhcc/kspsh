98b7a1c401e92cb327ff323864f3a360b1beb37f
Kirill Goncharov <kdgoncharov@yandex.ru>
2014-10-24 02:21:46 +0400

    Dynamic colors

--- vis/graphics.py
+++ vis/graphics.py
@@ -1,3 +1,4 @@
+import colorsys
 import logging
 import math
 import multiprocessing
@@ -18,7 +19,6 @@ class Visualizer(multiprocessing.Process):
         self.generator = source.spectrum_generator(delay)
         self.width = width
         self.height = height
-        self.color = code_to_rgb256('#C1FF8B')
 
     def run(self):
         logger.info('visualizer started')
@@ -30,7 +30,7 @@ class Visualizer(multiprocessing.Process):
             for i in range(block_size):
                 x = math.floor(i * self.width / block_size)
                 y = math.floor(spectrum[i // 2] * (self.height - 1))
-                mtx[x, y] = self.color
+                mtx[x, y] = value_to_rgb256(current_time + i / block_size)
             image = Image.fromarray(numpy.rot90(mtx), 'RGB')
             self.queue.put((current_time, image))
 
@@ -40,3 +40,23 @@ def code_to_rgb256(code):
     g = int(code[3:5], 16)
     b = int(code[5:7], 16)
     return r, g, b
+
+
+def rgb_to_rgb256(r, g, b):
+    r_ = math.floor(r * 255)
+    g_ = math.floor(g * 255)
+    b_ = math.floor(b * 255)
+    return r_, g_, b_
+
+
+def value_to_rgb256(value):
+    """
+    Translate arbitrary value to color
+    Accepts:
+        value: float
+    Returns:
+        (r, g, b) tuple
+    """
+    hsv = 0.8 + 0.2 * math.sin(value), 0.5, 1
+    rgb = colorsys.hsv_to_rgb(*hsv)
+    return rgb_to_rgb256(*rgb)
